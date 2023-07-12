cd2604838ad34496105a00d353c7d1f0e4339607
cristhian Villamarin <cristhianvillamarin2002@gmail.com>
2023-07-12 09:54:30 -0500

    actualizar repositorio

--- src/app/pages/main-pages/calendario/calendario.component.ts
+++ src/app/pages/main-pages/calendario/calendario.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef  } from '@angular/core';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import esLocale from '@fullcalendar/core/locales/es';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 
 @Component({
@@ -11,11 +11,11 @@ import esLocale from '@fullcalendar/core/locales/es';
 })
 export class CalendarioComponent implements OnInit{
 
-  public events: any[] = [];
+  public events: EventApi[] = [];
 
   public options: any;
 
-  constructor(){
+  constructor(private changeDetector: ChangeDetectorRef){
 
   }
 
@@ -24,7 +24,6 @@ export class CalendarioComponent implements OnInit{
     this.options = {
       plugins: [dayGridPlugin],
       defaultDate: new Date(),
-      locale: esLocale,
       header:{
         left: 'prev,next',
         center: 'title',
@@ -33,24 +32,58 @@ export class CalendarioComponent implements OnInit{
       editable: false
     }
 
-    this.events = [
-      {
-        title: "Evento 1",
-        start: new Date(),
-        description: "Evento 1"
-      },
-      {
-        title: "Evento 2",
-        start: new Date(new Date().getTime() + 89391),
-        description: "Evento 2"
-      },
-      {
-        title: "Evento 2",
-        start: new Date(),
-        end: new Date(new Date().getTime() + 80000 ),
-        description: "Evento 2"
-      }
-    ]
+    // this.events = [
+    //   {
+    //     title: "Evento 1",
+    //     start: new Date(),
+    //     description: "Evento 1"
+    //   },
+    //   {
+    //     title: "Evento 2",
+    //     start: new Date(new Date().getTime() + 89391),
+    //     description: "Evento 2"
+    //   },
+    //   {
+    //     title: "Evento 2",
+    //     start: new Date(),
+    //     end: new Date(new Date().getTime() + 80000 ),
+    //     description: "Evento 2"
+    //   }
+    // ]
+  }
+
+
+  handleWeekendsToggle() {
+    const { options } = this;
+    options.weekends = !options.weekends;
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.events = events;
+    this.changeDetector.detectChanges();
   }
 
 }

d6830a63127b7bca1b6f9cb0f5bf80997ba41641
cristhian Villamarin <cristhianvillamarin2002@gmail.com>
2023-07-13 16:53:16 -0500

    agregar algunas cosas del calendario y estilos

--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent {
+  classList: any;
+
+  Responsivo(){
+        // add hovered class to selected list item
+        let list = document.querySelectorAll(".navigation1 li");
+    
+        const activeLink = () => {
+          list.forEach((item) => {
+            item.classList.remove("hovered");
+          });
+          this.classList.add("hovered");
+        }
+        
+        let navigation: Element | null;
+        list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+        // Menu Toggle
+        let toggle: Element | null;
+        toggle = document.querySelector(".toggle1");
+        navigation = document.querySelector(".navigation1");
+        let main = document.querySelector(".main1");
+        console.log("hola");
+
+        if (navigation !== null) {
+          navigation.classList.toggle("active");
+        }
+
+        if (main !== null) {
+          main.classList.toggle("active");
+        }
+
+  }
 
 }
